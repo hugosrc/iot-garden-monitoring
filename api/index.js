2763eb35ddd7233dd4457625b6306407df948c6d
Hugo Souza <hugosr.contato@gmail.com>
2024-01-15 17:38:25 -0300

    feat: update dynamodb table and attributes names

--- api/index.js
+++ api/index.js
@@ -5,32 +5,32 @@ const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (event) => {
-    try {
-        const data = event.data;
-        
-        await Promise.all(data.map(async (garden) => {
-            const command = new PutCommand({
-                TableName: 'GardenMetrics',
-                Item: {
-                    gardenId: garden.id,
-                    timestamp: new Date(garden.timestamp * 1000).toISOString(),
-                    temperature: garden.temperature,
-                    humidity: garden.humidity,
-                }
-              });
-
-            await docClient.send(command);
-        }));
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Lambda function executed successfully' })
-        };
-    } catch (error) {
-        console.error(`Error processing event: ${JSON.stringify(event)}`, error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' })
-        };
-    }
+  try {
+    const data = event.data;
+    
+    await Promise.all(data.map(async (garden) => {
+      const command = new PutCommand({
+        TableName: 'IoTMonitoringGardenMetrics',
+        Item: {
+          GardenId: garden.id,
+          Timestamp: new Date(garden.timestamp * 1000).toISOString(),
+          Temperature: garden.temperature,
+          Humidity: garden.humidity,
+        }
+      });
+      
+      await docClient.send(command);
+    }));
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Lambda function executed successfully' })
+    };
+  } catch (error) {
+    console.error(`Error processing event: ${JSON.stringify(event)}`, error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' })
+    };
+  }
 };
